6421ef92b9318d07ada51ad855de72e33485fc5e
Milann SHRESTHA <31887847+milannshrestha@users.noreply.github.com>
2022-07-26 09:09:41 +0545

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import requests
 TOKEN = "XXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 chat_id = "XXXXXXXXX"
-text = " Hi, Im from CyberDefender Walkthrough :):) " #Send me a sweet messege if you use this script, :))
+text = " Hi, Im from CyberDefender Walkthrough :):) " #Send me sweet messege and I'll post it on my Twitter @milannshrestga:))
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={text}"
 
